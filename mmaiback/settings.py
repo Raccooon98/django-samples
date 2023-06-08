153493cf2378f018999c5ec6e12a30ccbda8f754
Raccooon98 <redbull1171@gmail.com>
2023-06-08 14:44:13 +0900

    cors header수정

--- mmaiback/settings.py
+++ mmaiback/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,6 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'rest_framework',
     'storages',
-    'corsheaders',
 ]
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
@@ -66,6 +66,7 @@ CKEDITOR_CONFIGS = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,7 +77,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mmaiback.urls'
-
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
                          ,'http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
